d28d69a3d0f84065709825f9ed3c859e5c518edd
lalinyuuu <lullalin.yu@gmail.com>
2025-09-11 13:59:21 +0700

    Enhance routing in App component: wrap application in BrowserRouter, update NotFound import path, and simplify Toaster usage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "@/components/main_layouts/nav_bar.jsx";
 import HeroSection from "@/components/main_layouts/hero_section.jsx";
 import ArticleSection from "@/feature/posts/containers/article_section.jsx";
 import ViewPostPage from "@/pages/post/view_post.jsx";
-import NotFound from "@/pages/not_found.jsx";
+import NotFound from "@/pages/post/not_found.jsx";
 import Footer from "@/components/main_layouts/footer.jsx";
 import { Toaster } from "sonner";
 
@@ -18,7 +18,7 @@ function Home() {
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,7 +26,7 @@ export default function App() {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
-      <Toaster richColors closeButton />
-    </>
+      <Toaster />
+    </BrowserRouter>
   );
 }
